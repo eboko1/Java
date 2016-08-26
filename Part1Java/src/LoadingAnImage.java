efe37784fd0809449fcf8e0f6e6719317afe8cb5
Victoria Frantsuh <eboko1@gmail.com>
2016-08-26 20:04:21 +0300

    add

---
+++ Part1Java/src/LoadingAnImage.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import comp102x.ColorImage;
+
+
+
+public class LoadingAnImage {
+
+	public void loadAnImage() {
+
+		// Please write your code after this line
+        // Please write your code after this line
+        ColorImage image = new ColorImage();
+        Canvas canvas = new Canvas(image.getWidth(), image.getHeight());
+        
+        //  load image to canvas
+        canvas.add(image);
+
+
+
+
+
+
+}
+
+}
