c236e4b47ce24f3e94512628ff5c110856c1310f
Victoria Frantsuh <eboko1@gmail.com>
2016-08-31 23:58:05 +0300

    add

--- MyFirstProject/src/Hello.java
+++ MyFirstProject/src/Hello.java
@@ -2,8 +2,15 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello VIKA!!!");// TODO Auto-generated method stub
-
+		
+		int a, kvadrat;
+		
+		System.out.println("Hello VIKA!!!"+);// TODO Auto-generated method stub
+  
+	}
+	public static int KV(int a){
+		kvadrat=a*a;
+		return kvadrat;
 	}
 
 }
