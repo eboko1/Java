17a1f86adce2b85f04cd8fcb0363cb7b6f132a6e
Victoria Frantsuh <eboko1@gmail.com>
2016-09-11 19:38:34 +0300

    formatter

--- ZhirniTomi/src/Formatterrr.java
+++ ZhirniTomi/src/Formatterrr.java
@@ -8,20 +8,18 @@ public class Formatterrr {
 	
 	public static void main(String[] args) {
 		try{
-			x= new Formatter("res//1.txt ");
+			x= new Formatter("res//2.txt ");
 			scnr=new Scanner(System.in);
 			
 			System.out.println("—к≥льки вам рок≥в?");
-			String years=scnr.next();
+			int years=(int)Double.parseDouble(scnr.next());			
 			System.out.println("як вас звати?");
 			String name= scnr.next();
 			System.out.println("ƒе ви проживаЇте?");
 			String gorod=scnr.next();
-			
-			x.format("ћене звати %s, мне %d, € проживаю в м≥ст≥ %s",years,name,gorod);
-		}catch(Exception e){
-			
-		}
+		    x.format("ћене звати %s, мне %d, € проживаю в м≥ст≥ %s",years,name,gorod);
+	        x.close();
+		}catch(Exception e){}
 	}
 
 }
