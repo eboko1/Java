fdc4b57135db1576eca1c129585ce7d52cc0037d
Victoria Frantsuh <eboko1@gmail.com>
2016-09-19 20:46:35 +0300

    comm st46

---
+++ MyFirstProject/src/Party.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+
+public class Party {
+
+	public static void main(String[] args) {
+		
+		
+		// void bildInvite(){
+		Frame f = new Frame();
+		Label l = new Label("Вечеря у Niku");
+		Button b = new Button("Ваша ставка");
+		Button c = new Button("Сбросить");
+		Panel p = new Panel();
+	
+		p.add(l);
+		p.add(b);
+		p.add(c);
+		p.add(f);
+		
+		}
+
+	}
