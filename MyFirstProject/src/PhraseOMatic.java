ad92695facaaa3228282b92cc2174bc6d6cbd578
Victoria Frantsuh <eboko1@gmail.com>
2016-09-21 17:49:13 +0300

    add Dog+Test

--- MyFirstProject/src/PhraseOMatic.java
+++ MyFirstProject/src/PhraseOMatic.java
@@ -1,4 +1,21 @@
+public class PhraseOMatic{
+public static void main (String[] args){
+String[] wordListOne={"kryhlosytochnuy","vzaemnuy","frontend"};
+String[] wordListTwo={"trydnuy","setevoy","yscorenuy"};
+String[] wordListThree={"proces","talant","obzor"};
 
-public class PhraseOMatic {
+int  oneLength=wordListOne.length;
+int  twoLength=wordListTwo.length;
+int  threeLength=wordListThree.length;
 
+
+
+int rand1= (int)(Math.random()*oneLength);
+int rand2= (int)(Math.random()*twoLength);
+int rand3= (int)(Math.random()*threeLength);
+
+String phrase=wordListOne[rand1]+" "+wordListTwo[rand2]+" "+wordListThree[rand3];
+
+System.out.println("Vse sho potribno, it is "+phrase);
+}
 }
